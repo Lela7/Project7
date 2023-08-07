a7a7b1aba22e7f2fc25c9c690aaf41574e8c4e2d
Danijela Bodnar <danijelabodnar@hotmail.com>
2023-08-07 16:12:11 +0200

    Izmena podataka u tabeli

--- Test7BACK/Test/src/main/resources/data.sql
+++ Test7BACK/Test/src/main/resources/data.sql
@@ -14,13 +14,13 @@ INSERT INTO format (id, tip, broj_ucesnika) VALUES (4, 'MASTERS 250', 250);
 
 
 
-INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (1, 'Australian Open', 'Melbourne, Australia', '22/03/2023', '29/03/2023', 1);
-INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (2, 'Australian Open1', 'Melburn','22/03/2023', '29/03/2023', 3);
-INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (3, 'Rome','Cikago', '22/03/2023', '29/03/2023', 1);
-INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (4, 'Cirkus Open','London', '22/03/2023', '29/03/2023', 4);
+INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (1, 'Australian Open', 'Melbourne, Australia', '2023/01/08', '2023/01/29', 1);
+INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (2, 'Italian Open', 'Rome, Italy','2023/05/08', '2023/05/21', 2);
+INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (3, 'US Open','NY, USA', '2023/08/21', '2023/09/10', 1);
+INSERT INTO takmicenje (id, naziv, mesto, pocetak, zavrsetak, format_id) VALUES (4, 'Miami Open','Miami, FLorida', '2023/03/22', '2023/04/02', 4);
 
 
-INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (1, 'Australija', 'zorica@mail.drop', '22/06/2023', 1);
-INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (2,'Francuska', 'milica@mail.drop', '19/09/2023', 3);
-INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (3, 'Italija', 'radoje@mail.drop', '12/09/2022', 2);
-INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (4, 'Nemacka', 'zoran@mail.drop', '08/08/2022', 4);
+INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (1, 'Australija', 'zorica@mail.drop', '2023/01/01', 1);
+INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (2,'Francuska', 'milica@mail.drop', '2023/01/03', 3);
+INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (3, 'Italija', 'radoje@mail.drop', '2023/01/15', 2);
+INSERT INTO prijava (id, drzava, kontakt, datum_prijave, takmicenje_id) VALUES (4, 'Nemacka', 'zoran@mail.drop', '2023/02/02', 4);
